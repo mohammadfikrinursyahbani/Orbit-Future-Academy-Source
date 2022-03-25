dda34d28dd64f14702be101f44360471d5d564db
Mohammad Fikri Nur Syahbani <72445001+mohammadfikrinursyahbani@users.noreply.github.com>
2022-03-25 08:58:11 +0700

    Porto

---
+++ Portofolio Web/script.js
@@ -0,0 +1,39 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/*-- About Tabs --*/
+const tabsContainer = document.querySelector(".about-tab"),
+aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+        tabsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const target = e.target.getAttribute("data-target");
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+        aboutSection.querySelector(target).classList.add("active");
+    }
+});
